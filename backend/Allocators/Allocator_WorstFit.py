a983f21fa6a915acfe283affdd1ecb445446c241
MohamedEshmawy <36339453+MohamedEshmawy@users.noreply.github.com>
2019-05-05 04:56:10 -0700

    added definition for three allocator classes

---
+++ backend/Allocators/Allocator_WorstFit.py
@@ -0,0 +1,12 @@
+from backend.data_containers.Memory import Memory
+import copy
+
+class Allocator():
+	def __init__(self, memory, withCompaction):
+		Allocator.__init__(self, memory, withCompaction)
+
+
+
+	def allocate()
+		#Do stuff here and return memory
+		
