99e9ead5210bdc61519b20728b404cbfff324e5b
MohamedEshmawy <36339453+MohamedEshmawy@users.noreply.github.com>
2019-05-05 04:52:32 -0700

    added Allocator parent class

---
+++ backend/Allocators/Allocator.py
@@ -0,0 +1,13 @@
+from backend.data_containers.Memory import Memory
+import copy
+
+class Allocator():
+	def __init__(self, memory, withCompaction):
+		self.memory = copy.deepcopy(memory)
+		self.withCompaction = withCompaction
+
+
+
+	def allocate()
+		pass
+		
